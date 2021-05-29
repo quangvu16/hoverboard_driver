a1e8d2d26485ef6ed64664b93bc5491aff589c57
quangvu16 <68116625+quangvu16@users.noreply.github.com>
2021-05-29 19:10:59 +0700

    Update hoverboard.cpp

--- src/hoverboard.cpp
+++ src/hoverboard.cpp
@@ -13,43 +13,20 @@ int serialWrite(unsigned char *data, int len) {
     return (int)write(port_fd, data, len);
 }
 
-void readCallback(PROTOCOL_STAT* s, PARAMSTAT* param, uint8_t fn_type, unsigned char* content, int len) {
-  if (fn_type == FN_TYPE_POST_READRESPONSE) {
-    if (param->code == HoverboardAPI::Codes::sensHall) {
-      Hoverboard::getInstance().hallCallback();
-    } else if (param->code == HoverboardAPI::Codes::sensElectrical) {
-      Hoverboard::getInstance().electricalCallback();
-    }
-  }
-}
-
-Hoverboard& Hoverboard::getInstance() {
-  static Hoverboard hoverboard;
-  return hoverboard;
-}
-
 Hoverboard::Hoverboard() {
-    hardware_interface::JointStateHandle left_wheel_state_handle("left_wheel",
-								 &joints[0].pos.data,
-								 &joints[0].vel.data,
-								 &joints[0].eff.data);
-    hardware_interface::JointStateHandle right_wheel_state_handle("right_wheel",
-								  &joints[1].pos.data,
-								  &joints[1].vel.data,
-								  &joints[1].eff.data);
+    hardware_interface::JointStateHandle left_wheel_state_handle("left_wheel", &joints[0].pos.data, &joints[0].vel.data, &joints[0].speed.data);
+    hardware_interface::JointStateHandle right_wheel_state_handle("right_wheel", &joints[1].pos.data, &joints[1].vel.data, &joints[1].speed.data);
     joint_state_interface.registerHandle (left_wheel_state_handle);
     joint_state_interface.registerHandle (right_wheel_state_handle);
     registerInterface(&joint_state_interface);
 
-    hardware_interface::JointHandle left_wheel_vel_handle(joint_state_interface.getHandle("left_wheel"),
-							  &joints[0].cmd.data);
-    hardware_interface::JointHandle right_wheel_vel_handle(joint_state_interface.getHandle("right_wheel"),
-							   &joints[1].cmd.data);
+    hardware_interface::JointHandle left_wheel_vel_handle(joint_state_interface.getHandle("left_wheel"), &joints[0].cmd.data);
+    hardware_interface::JointHandle right_wheel_vel_handle(joint_state_interface.getHandle("right_wheel"), &joints[1].cmd.data);
     velocity_joint_interface.registerHandle (left_wheel_vel_handle);
     velocity_joint_interface.registerHandle (right_wheel_vel_handle);
     registerInterface(&velocity_joint_interface);
 
-    // These publishers are only for debugging purposes
+     // These publishers are only for debugging purposes
     left_pos_pub  = nh.advertise<std_msgs::Float64>("hoverboard/left_wheel/position", 3);
     right_pos_pub = nh.advertise<std_msgs::Float64>("hoverboard/right_wheel/position", 3);
     left_vel_pub  = nh.advertise<std_msgs::Float64>("hoverboard/left_wheel/velocity", 3);
@@ -87,19 +64,31 @@ Hoverboard::Hoverboard() {
 
     // Support dynamic reconfigure
     dsrv = new dynamic_reconfigure::Server<hoverboard_driver::HoverboardConfig>(ros::NodeHandle("~"));
-    dynamic_reconfigure::Server<hoverboard_driver::HoverboardConfig>::CallbackType cb =
-      boost::bind(&Hoverboard::reconfigure_callback, this, _1, _2);
+    dynamic_reconfigure::Server<hoverboard_driver::HoverboardConfig>::CallbackType cb = boost::bind(&Hoverboard::reconfigure_callback, this, _1, _2);
     dsrv->setCallback(cb);
 
-    api->updateParamHandler(HoverboardAPI::Codes::sensHall, readCallback);
-    api->updateParamHandler(HoverboardAPI::Codes::sensElectrical, readCallback);
 
-    api->disablePoweroffTimer();
+//   api->updateParamHandler(HoverboardAPI::Codes::sensHall, readCallback);
+//   api->updateParamHandler(HoverboardAPI::Codes::sensElectrical, readCallback);
+
+ //   api->disablePoweroffTimer();
+
+    std_msgs::Float64 f;
+
+    f.data = api->getMotorAmpsAvg(0);
+    left_cur_pub.publish(f);
+
+    f.data = api->getMotorAmpsAvg(1);
+    right_cur_pub.publish(f);
+
+    f.data = api->getBatteryVoltage();
+    voltage_pub.publish(f);
 }
 
 Hoverboard::~Hoverboard() {
     if (port_fd != -1) 
         close(port_fd);
+
     delete api;
 }
 
@@ -117,49 +106,53 @@ void Hoverboard::read() {
         unsigned char c;
         int i = 0, r = 0;
 
-        while ((r = ::read(port_fd, &c, 1)) > 0 && i++ < 1024)
+        while ((r = ::read(port_fd, &c, 1)) > 0 && i++ < 1024) {
             api->protocolPush(c);
+        }
 
-	if (i > 0)
+	if (i > 0) {
 	  last_read = ros::Time::now();
+	}
 
-	if (r < 0 && errno != EAGAIN)
+	if (r < 0 && errno != EAGAIN) {
 	  ROS_ERROR("Reading from serial %s failed: %d", PORT, r);
+	}
     }
 
     if ((ros::Time::now() - last_read).toSec() > 1) {
       ROS_FATAL("Timeout reading from serial %s failed", PORT);
     }
-}
 
-void Hoverboard::hallCallback() {
     // Convert m/s to rad/s
-    double sens_speed0 = api->getSpeed0_mms();
-    double sens_speed1 = api->getSpeed1_mms();
+    sens_speed0 = api->getSpeed0_mms();
+    sens_speed1 = api->getSpeed1_mms();
 
-    joints[0].vel.data = DIRECTION_CORRECTION * (sens_speed0 / 1000.0);
-    joints[1].vel.data = DIRECTION_CORRECTION * (sens_speed1 / 1000.0);
-    joints[0].pos.data = DIRECTION_CORRECTION * (api->getPosition0_mm() / 1000.0);
-    joints[1].pos.data = DIRECTION_CORRECTION * (api->getPosition1_mm() / 1000.0);
-    left_vel_pub.publish(joints[0].vel);
-    right_vel_pub.publish(joints[1].vel);
-    left_pos_pub.publish(joints[0].pos);
-    right_pos_pub.publish(joints[1].pos);
+    //    printf("[%.3f] ", ros::Time::now().toSec());
 
-    // printf("[%.3f, %.3f] -> [%.3f, %.3f]\n", sens_speed0, sens_speed1, joints[0].vel.data, joints[1].vel.data);
-}
-
-void Hoverboard::electricalCallback() {
-    std_msgs::Float64 f;
-
-    f.data = api->getMotorAmpsAvg(0);
-    left_cur_pub.publish(f);
-
-    f.data = api->getMotorAmpsAvg(1);
-    right_cur_pub.publish(f);
-
-    f.data = api->getBatteryVoltage();
-    voltage_pub.publish(f);
+    // Basic sanity check, speed should be less than 10 m/s
+    // Sometimes, it seems during EMI peaks, we're getting ridiculous values here
+    // Don't know what to do with it, just ignoring for now
+    if (fabs(sens_speed0) < 10000 && fabs(sens_speed1) < 10000) { 
+      joints[0].vel.data = DIRECTION_CORRECTION * (sens_speed0 / 1000.0) / wheel_radius;
+      joints[1].vel.data = DIRECTION_CORRECTION * (sens_speed1 / 1000.0) / wheel_radius;
+      joints[0].pos.data = DIRECTION_CORRECTION * (api->getPosition0_mm() / 1000.0) / wheel_radius;
+      joints[1].pos.data = DIRECTION_CORRECTION * (api->getPosition1_mm() / 1000.0) / wheel_radius;
+      
+      left_vel_pub.publish(joints[0].vel);
+      right_vel_pub.publish(joints[1].vel);
+      left_pos_pub.publish(joints[0].pos);
+      right_pos_pub.publish(joints[1].pos);
+      
+      printf("[%.3f, %.3f] -> [%.3f, %.3f]\n", sens_speed0, sens_speed1, joints[0].vel.data, joints[1].vel.data);
+      
+    } else {
+      printf(" Stupid speeds of [%.2f, %.2f] ignored\n", sens_speed0, sens_speed1);
+    }
+    //  speed_msg.header.stamp = nh.now();      	//timestamp for odometry data
+  //speed_msg.vector.x = sens_speed0;    	//left wheel speed (in m/s)
+  //speed_msg.vector.y = sens_speed1;   	//right wheel speed (in m/s)
+  //speed_msg.vector.z = 100/1000;         	//looptime, should be the same as specified in LOOPTIME (in s)
+  //speed_pub.publish(&speed_msg);
 }
 
 void Hoverboard::write() {
@@ -167,6 +160,7 @@ void Hoverboard::write() {
         ROS_ERROR("Attempt to write on closed serial");
         return;
     }
+
     // Inform interested parties about the commands we've got
     left_cmd_pub.publish(joints[0].cmd);
     right_cmd_pub.publish(joints[1].cmd);
@@ -180,7 +174,9 @@ void Hoverboard::write() {
     // Convert rad/s to m/s
     double left_speed = DIRECTION_CORRECTION * joints[0].cmd.data * wheel_radius;
     double right_speed = DIRECTION_CORRECTION * joints[1].cmd.data * wheel_radius;
-    const int max_power = have_config ? config.MaxPwr : 100;    
+
+    // Cap according to dynamic_reconfigure
+    const int max_power = have_config ? config.MaxPwr : 100;
     const int min_speed = have_config ? config.MinSpd : 40;
     api->sendSpeedData (left_speed, right_speed, max_power, min_speed);
 #endif
@@ -188,4 +184,5 @@ void Hoverboard::write() {
 
 void Hoverboard::tick() {
     api->protocolTick();
+    //    api->printStats();
 }
